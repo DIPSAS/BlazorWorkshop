6651503cdb85027854b10ba99d4d27e7039b174d
Isak Sunde Singh <isak.sunde.singh@gmail.com>
2019-09-06 13:45:49 +0200

    Add basic pizza-ordering functionality

---
+++ Workshop.Server/SpecialsController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Workshop.Server
+{
+    [Route("specials")]
+    [ApiController]
+    public class SpecialsController : Controller
+    {
+        private readonly PizzaStoreContext _db;
+
+        public SpecialsController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PizzaSpecial>>> GetSpecials()
+        {
+            return (await _db.Specials.ToListAsync()).OrderByDescending(sp => sp.BasePrice).ToList();
+        }
+    }
+}
---
+++ Workshop.Shared/UserInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Workshop
+{
+    public class UserInfo
+    {
+        public bool IsAuthenticated { get; set; }
+
+        public string Name { get; set; }
+    }
+}

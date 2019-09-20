49e75a3470451d33ec91cc30682b74b39e724ff3
Mads Johansen <johansen.max@gmail.com>
2019-09-20 10:42:39 +0200

    Add more drugs

--- Workshop.Server/SeedData.cs
+++ Workshop.Server/SeedData.cs
@@ -11,24 +11,43 @@
                 {
                     Name = "The pain reliever",
                     Description = "It makes the booboo go away!",
-                    BasePrice = 9.99m,
+                    BasePrice = 20.00m,
                     ImageUrl = "img/drugs/vicodin.jpg",
                 },
                 new DrugSpecial()
                 {
-                    Id = 2,
                     Name = "The collection",
                     Description = "It has all the drugs!",
-                    BasePrice = 11.99m,
+                    BasePrice = 99.99m,
                     ImageUrl = "img/drugs/pill-sampler.jpg",
                 },
                 new DrugSpecial()
                 {
-                    Id = 3,
                     Name = "The big one",
                     Description = "Control your diabetes in 1-2-3",
-                    BasePrice = 10.50m,
+                    BasePrice = 25.00m,
                     ImageUrl = "img/drugs/insulin.jpg",
+                },
+                new DrugSpecial()
+                {
+                    Name = "Purple drank",
+                    Description = "Soothing action for your head cold",
+                    BasePrice = 49.00m,
+                    ImageUrl = "img/drugs/purple-drank.jpg",
+                },
+                new DrugSpecial()
+                {
+                    Name = "Off-brand opiods",
+                    Description = "The scourge of the midwest",
+                    BasePrice = 50.00m,
+                    ImageUrl = "img/drugs/pain-killers.jpg",
+                },
+                new DrugSpecial()
+                {
+                    Name = "Holistic Placebos",
+                    Description = "Beloved by Facebook aunts",
+                    BasePrice = 75.00m,
+                    ImageUrl = "img/drugs/natural.jpg",
                 }
             };
 
